7b25ba73c5dcd626c23a8642626a7d76676fda60
George Durden <george@durden.com>
2022-12-08 16:58:07 -0500

    Update Imports to use Import Map

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import * as THREE from "https://unpkg.com/three@0.147.0/build/three.module.js";
-import { OrbitControls } from "https://unpkg.com/three@0.147.0/examples/jsm/controls/OrbitControls.js";
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 let container, scene, camera, renderer, card, controls;
 
